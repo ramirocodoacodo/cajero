3ee596f5b682c977ca721966d8048676d512452b
imoreyra <alumno@DESKTOP-N2H88CC>
2018-07-10 18:38:14 -0300

    Se crean métodos de obtención de listas de Tipos de Movimiento y Tipos de Cuuenta en CajeroController

--- Cajero/src/ar/cac/cajero/controller/CajeroController.java
+++ Cajero/src/ar/cac/cajero/controller/CajeroController.java
@@ -5,10 +5,29 @@
  */
 package ar.cac.cajero.controller;
 
+import ar.cac.cajero.model.TiposCuenta;
+import ar.cac.cajero.model.TiposMovimiento;
+import ar.cac.cajero.util.CajeroHibernateUtil;
+import java.util.ArrayList;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author alumno
  */
 public class CajeroController {
-    
+
+    public ArrayList<TiposCuenta> getTiposCuenta() {
+        ArrayList<TiposCuenta> tiposDeCuenta = new ArrayList<>();
+        SessionFactory mySessionFactory = CajeroHibernateUtil.getSessionFactory();
+        tiposDeCuenta.addAll(mySessionFactory.getCurrentSession().createCriteria(TiposCuenta.class).list());
+        return tiposDeCuenta;
+    }
+
+    public ArrayList<TiposMovimiento> getTiposMovimiento() {
+        ArrayList<TiposMovimiento> tiposDeMovimiento = new ArrayList<>();
+        SessionFactory mySessionFactory = CajeroHibernateUtil.getSessionFactory();
+        tiposDeMovimiento.addAll(mySessionFactory.getCurrentSession().createCriteria(TiposMovimiento.class).list());
+        return tiposDeMovimiento;
+    }
 }
